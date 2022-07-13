bcba56a49e92e88639bc11903f3e1dde027d492d
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-07-13 18:20:06 +0200

    creato api

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function index(){
-        $posts = Post::where('category')->where('')
+        $posts = Post::with('category')->with('tags')->get();
+        return response()->json($posts);
     }
 }
